ae260be79b2253dd4c45ae58d0db4c28655047ac
lolito <prof.lolito@gmail.com>
2024-01-06 17:35:09 +0200

    BackEnd Register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,55 @@
-const registerUser = (req, res, next) => {
-    res.json('Register User')
-}
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModels');
+const HttpError = require('../models/errorModel');
 
-const loginUser = (req, res, next) => {
-    res.json('Login User')
-}
+// Unprotected
+const registerUser = async (req, res, next) => {
+    try {
+        const { name, email, password, password2 } = req.body;
+        if(!name || !email || !password) {
+            return next(new HttpError('Fill in all fields.', 422));
+        }
+        const newEmail = email.toLowerCase();
+        const emailExists = await User.findOne({ email: newEmail })
+        if(emailExists) {
+            return next(new HttpError('Email already exists.', 422))
+        }
 
-const getUser = (req, res, next) => {
+        if((password.trim()).length < 6 ) {
+            return next(new HttpError('Password should be at least 6 characters.', 422))
+        }
+        if(password != password2 ) {
+            return next(new HttpError('Passwords do no match.', 422))
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = await User.create({name, email, newEmail, password: hashedPassword})
+        res.status(201).json(`New user ${newUser.email} registered`)
+
+    } catch (error) {
+        return next(new HttpError('User registration failed', 422));
+    }
+};
+
+const loginUser = async (req, res, next) => {
+    res.json('Login User');
+};
+
+const getUser = async (req, res, next) => {
     res.json('Get User')
-}
+};
 
-const changeAvatar = (req, res, next) => {
+const changeAvatar = async (req, res, next) => {
     res.json('Change User Avatar')
-}
+};
 
-const editUser = (req, res, next) => {
+const editUser = async (req, res, next) => {
     res.json('Edit User Details')
-}
+};
 
-const getAuthors = (req, res, next) => {
+const getAuthors = async (req, res, next) => {
     res.json('Get Authors')
-}
+};
 
 module.exports = { registerUser, loginUser, getUser, changeAvatar,editUser, getAuthors}
