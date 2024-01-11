8ebb5e11b14b445c201e71b4b6cd3122d2627a96
lolito <prof.lolito@gmail.com>
2024-01-11 16:24:32 +0200

    Uploads

--- models/postModel.js
+++ models/postModel.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const postSchema = new Schema({
     title: { type: String, required: true },
-    category: { type: String, enum: ['Data Science', 'Web Development', 'General', 'Web Of Things', 'Education', 'Art', 'Photography', 'Entertainment', 'Business', 'Weather'], message: '{VALUE is not supported}' },
+    category: { type: String, enum: ['Robotics', 'Web Development', 'General', 'Web Of Things', 'Data Science', 'Art', 'Education', 'Design', 'Weather'], message: '{VALUE is not supported}' },
     content: { type: String, required: true },
     author: {type: Schema.Types.ObjectId, ref: 'Users'},
     thumbnail:{ type: String, required: true },
