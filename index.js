3b24c90b9d936a31279b70999fb3b7fc17f7cd57
radlee <prof.lolito@gmail.com>
2024-02-04 22:14:10 +0200

    Conditional api dev or local

--- index.js
+++ index.js
@@ -13,8 +13,16 @@ const { notFound, errorHandler } = require('./middlewares/errorMiddleware')
 const app = express();
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
-// app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
-app.use(cors({credentials: true, origin: 'https://radblokmultimedia.onrender.com'}))
+
+// Determine CORS origin based on the environment
+const corsOptions = {
+    credentials: true,
+    origin: process.env.NODE_ENV === 'production' 
+      ? 'https://radblokmultimedia.onrender.com' 
+      : 'http://localhost:3000'
+  };
+app.use(cors(corsOptions));
+
 app.use(upload());
 app.use('uploads', express.static(__dirname + '/uploads'))
 
