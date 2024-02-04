b6da9a53e7cf6a1bb7ea985c1ae5f25fc185d19b
lolito <prof.lolito@gmail.com>
2024-02-04 13:27:22 +0200

    Fixed Cats names

--- models/postModel.js
+++ models/postModel.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const postSchema = new Schema({
     title: { type: String, required: true },
-    category: { type: String, enum: ['Robotics', 'Web Development', 'General', 'Web Of Things', 'Data Science', 'Art', 'Education', 'Design', 'Weather'], message: '{VALUE is not supported}' },
+    category: { type: String, enum: ['Robotics','Technology', 'Business', 'Literature', 'General', 'Programming', 'Life Style', 'Design', 'Sports'], message: '{VALUE is not supported}' },
     content: { type: String, required: true },
     author: {type: Schema.Types.ObjectId, ref: 'Users'},
     thumbnail:{ type: String, required: true },
