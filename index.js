aa21ec03ca7e585b5cdb9c2e3b7bdbc31de5fffd
radlee <prof.lolito@gmail.com>
2024-10-29 03:19:49 +0200

    Updated the API routes

--- index.js
+++ index.js
@@ -6,33 +6,47 @@ const upload = require('express-fileupload');
 
 const PORT = process.env.PORT;
 
-const userRoutes = require('./routes/userRoutes')
-const postRoutes = require('./routes/postRoutes')
-const { notFound, errorHandler } = require('./middlewares/errorMiddleware')
+const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 const app = express();
-app.use(express.json({extended: true}))
-app.use(express.urlencoded({extended: true}))
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // Determine CORS origin based on the environment
 const corsOptions = {
-    credentials: true,
-    origin: process.env.NODE_ENV === 'production' 
-      ? 'https://radblokmultimedia.onrender.com' 
-      : 'http://localhost:3000'
-  };
+  credentials: true,
+  origin:
+    process.env.NODE_ENV === 'production'
+      ? 'https://radblokmultimedia.onrender.com'
+      : 'http://localhost:3000',
+};
 app.use(cors(corsOptions));
 
 app.use(upload());
-app.use('uploads', express.static(__dirname + '/uploads'))
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
-app.use('/api/users/', userRoutes)
-app.use('/api/posts/', postRoutes)
+// Define a default route for `/api`
+app.get('/api', (req, res) => {
+  res.send('Welcome to radblok API');
+});
 
+// Updated routes without trailing slashes
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+// Error handling middleware
+app.use(notFound);
+app.use(errorHandler);
 
-connect(process.env.MONGO_URI).then(app.listen(PORT, () => 
-console.log(`Running on PORT: ${PORT}\nDatabase Connected Successfully!`)))
-.catch(error => {console.log(error)})
+// Database connection and server start
+connect(process.env.MONGO_URI)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Running on PORT: ${PORT}\nDatabase Connected Successfully!`)
+    )
+  )
+  .catch((error) => {
+    console.log(error);
+  });
