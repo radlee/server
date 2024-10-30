348f8443b2facf515aa5dd8d3c726a2cd7b9fdf5
radlee <prof.lolito@gmail.com>
2024-10-31 01:21:24 +0200

    Updated the category personal dev to Growth

--- models/postModel.js
+++ models/postModel.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const postSchema = new Schema({
     title: { type: String, required: true },
-    category: { type: String, enum: ['General','Health and Wellness', 'Personal Development', 'Technology', 'Finance', 'Career and Business', 'Lifestyle', 'Travel', 'Food and Cooking'], message: '{VALUE is not supported}' },
+    category: { type: String, enum: ['General','Health and Wellness', 'Growth', 'Technology', 'Finance', 'Career and Business', 'Lifestyle', 'Travel', 'Food and Cooking'], message: '{VALUE is not supported}' },
     content: { type: String, required: true },
     author: {type: Schema.Types.ObjectId, ref: 'User'},
     thumbnail:{ type: String, required: true },
